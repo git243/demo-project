20668be6cd7904000d0a5c8ba799b1eddb8a227e
Xin Yan Lee <xinyan.lee@merquri.io>
2025-05-19 09:52:50 +0800

    back to origin

--- hello.py
+++ hello.py
@@ -1,12 +1,3 @@
-def wrong_function2():
-    print("hehehehe22222h")
-
-def wrong_function():
-    print("asdadasdasdasdasdasdasdasdasd")
-
-def test_function():
-    print("this is the test function 1 yoh.")
-
 if __name__ == 'main':
     print('Hello world')
 else:
